e4e8a02bde42880964385e8942d26881662d5cc8
mdunaevs <44279139+mdunaevs@users.noreply.github.com>
2018-11-26 17:10:27 -0500

    Add files via upload
    I am creating a card game called Sequence

---
+++ sequence_client.py
@@ -0,0 +1,201 @@
+import socket
+import threading
+from queue import Queue
+import json
+
+HOST = "" # put your IP address here if playing on multiple computers
+PORT = 10075
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.connect((HOST,PORT))
+print("connected to server")
+
+def handleServerMsg(server, serverMsg):
+    print("recieved from server")
+    server.setblocking(1)
+    msg = ""
+    command = ""
+    while True:
+        msg += server.recv(1024).decode("UTF-8")
+        command = msg.split("\n")
+        while (len(command) > 1):
+            readyMsg = command[0]
+            msg = "\n".join(command[1:])
+            serverMsg.put(readyMsg)
+            command = msg.split("\n")
+
+# events-example0.py from 15-112 website
+# Barebones timer, mouse, and keyboard events
+
+from tkinter import *
+from sequence import *
+import random
+####################################
+# customize these functions
+####################################
+
+def init(data):
+    data.d1 = Deck()
+    data.d2 = Deck()
+##    data.d1.cardFillDeck()
+##    data.d2.cardFillDeck()
+    data.cardBoard = CardBoard()
+    data.cardBoard.cardFillBoard()
+    data.playerCards = PlayerDeck(data.d1, data.d2)
+    data.otherPlayers = dict()
+    data.gameOver = False
+    data.playedTurn = False
+    data.receivedCard = False
+    data.pBoard = PieceBoard()
+    data.playerID = 0
+    data.getCardBtn = NewCardBtn(data.width - 2 * Card.cardWidth, \
+                                 data.height // 2)
+
+def endTurnClicked(data, x, y):
+    return (data.width - 50 <= x <= data.width and\
+            data.height - 50 <= y <= data.height)
+
+def mousePressed(event, data):
+    msg = ""
+    row,col = data.pBoard.convertCoordToPos(event.x, event.y)
+    if(not data.playedTurn):
+        if(data.pBoard.onPieceBoard(row, col) and\
+           data.pBoard.isValidPos(row, col) and\
+           (data.playerCards.hasCard(data.cardBoard.getCard(row, col)) or\
+            data.playerCards.hasTwoEyedJack())):
+            data.playedTurn = True
+            data.playerCards.removeCard(data.cardBoard.getCard(row, col))
+            data.pBoard.fillPosInPieceBoard(row, col, \
+                                            data.playerID)
+            
+            
+            #data.cardBoard.getCard(row,col).convertColor()
+            #data.cardBoard.modifyCardColor(row, col, \
+                                           #data.players[data.playerInd])
+##            data.cardBoard.getCard(row, col).convertColor(\
+##                data.players[data.playerInd])
+            data.pBoard.printBoard()
+            print()
+            #msg = "playerPlayed "+json.dumps(data.pBoard)
+            msg = "playerPlayed " + str(data.pBoard)
+            print(msg)
+        elif(data.pBoard.onPieceBoard(row, col) and\
+             not data.pBoard.isValidPos(row, col) and\
+             data.playerCards.hasOneEyedJack() and\
+             data.playerID != data.pBoard.getPlayer(row, col)):
+            data.playedTurn = True
+            data.playerCards.removeCard(data.cardBoard.getCard(row, col))
+            data.pBoard.fillPosInPieceBoard(row, col, 0)
+            #msg = "playerPlayed "+json.dumps(data.pBoard)
+            msg = "playerPlayed " + str(data.pBoard)
+            print(msg)
+        elif(data.getCardBtn.buttonClicked(event.x, event.y) and \
+             not data.receivedCard):
+            print("player wants new card")
+            getCardBtn.buttonAction(data.playerCards, data.d1, data.d2)
+            data.receivedCard = True
+        elif(endTurnClicked(data, event.x, event.y)):
+            data.playedTurn = False
+            data.receivedCard = False
+            msg = "playerEnded"
+            print(msg)
+        if(msg != ""):
+            data.server.send(msg.encode())
+
+def keyPressed(event, data):
+    pass
+
+def timerFired(data):
+    while (serverMsg.qsize() > 0):
+        msg = serverMsg.get(False)
+        
+        try:
+            print("received: ", msg, "\n")
+            msg = msg.split()
+            command = msg[0]
+            if(command == "myIDis"):
+                data.playerID = msg[1]
+            elif(command == "newPlayer"):
+                newPID = msg[1]
+                data.otherPlayers[newPID] = PlayerDeck(data.d1, data.d2)
+            elif(command == "playerPlayed"):
+                if(data.pBoard.winningBoard(0, 0)):
+                    data.gameOver = True
+            elif(command == "boardFilled"):
+                print("This is the message from server to redraw")
+                # refill players boards
+                print(msg[1:])
+                data.pBoard.refillPBoard(msg[1:])
+            elif(command == "playerEnded"):
+                # Transfers move to next player
+                print("Player ended turn")
+        except:
+            print("failed")
+            serverMsg.task_done()
+
+def redrawAll(canvas, data):
+    if(data.gameOver):
+        canvas.create_rectangle(0, 0, data.width, data.height, fill = "red")
+    else:
+        #canvas.create_image(0, 0, anchor = NW, \
+        #                    image = data.pokerTableImg)
+        data.cardBoard.drawBoard(canvas)
+        ## Temp State ##
+        data.pBoard.drawPieces(canvas)
+        ## END ## 
+        data.playerCards.drawDeck(canvas)
+        data.getCardBtn.drawBtn(canvas)
+        canvas.create_rectangle(data.width - 50, data.height - 50, \
+                                data.width, data.height)
+
+####################################
+# use the run function as-is
+####################################
+
+def run(width, height, serverMsg=None, server=None):
+    def redrawAllWrapper(canvas, data):
+        canvas.delete(ALL)
+        redrawAll(canvas, data)
+        canvas.update()
+
+    def mousePressedWrapper(event, canvas, data):
+        mousePressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def keyPressedWrapper(event, canvas, data):
+        keyPressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def timerFiredWrapper(canvas, data):
+        timerFired(data)
+        redrawAllWrapper(canvas, data)
+        # pause, then call timerFired again
+        canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+    # Set up data and call init
+    class Struct(object): pass
+    data = Struct()
+    data.server = server
+    data.serverMsg = serverMsg
+    data.width = width
+    data.height = height
+    data.timerDelay = 100 # milliseconds
+    # create the root and the canvas
+    root = Tk()
+    canvas = Canvas(root, width=data.width, height=data.height)
+    canvas.pack()
+    init(data)
+    # set up events
+    root.bind("<Button-1>", lambda event:
+                            mousePressedWrapper(event, canvas, data))
+    root.bind("<Key>", lambda event:
+                            keyPressedWrapper(event, canvas, data))
+    timerFiredWrapper(canvas, data)
+    # and launch the app
+    root.mainloop()  # blocks until window is closed
+    print("bye!")
+
+serverMsg = Queue(100)
+threading.Thread(target = handleServerMsg, args = (server, serverMsg)).start()
+
+run(1400, 810, serverMsg, server)
---
+++ sequence_server.py
@@ -0,0 +1,122 @@
+import socket
+import threading
+from queue import Queue
+from sequence import *
+import json
+
+HOST = "" # Empty is own computer # put your IP address here if playing on multiple computers
+PORT = 10075 # Change each time you test
+BACKLOG = 3 # number of people
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.bind((HOST,PORT))
+server.listen(BACKLOG)
+print("looking for connection")
+
+def fillPBoard(msg):
+    print("This is how the board looks before filling: ")
+    pBoard.printBoard()
+    msg = msg.split(" [[")
+    matrixMsg = msg[1]
+    print("This is inside of filling only matrix list: ", matrixMsg)       
+    matrixMsg = matrixMsg.replace("], [", ", ")
+    matrixMsg = matrixMsg.replace("]]", "")
+    print("This is inside of filling only matrix list cleaned: ", matrixMsg)
+    matrixMsg = matrixMsg.split(", ")
+    print("This is inside of filling only matrix: ", matrixMsg)
+    counter = 0
+    matrixLen = 10
+    for row in range(matrixLen):
+        print("inside row")
+        for col in range(matrixLen):
+            print("inside col")
+            print("(", row, col, "): ", pBoard.getPlayer(row, col))
+            print(type(pBoard.getPlayer(row, col)))
+            if(pBoard.getPlayer(row, col) != '0'):
+                pBoard.setPlayer(row, col, pBoard.getPlayer(row, col))
+            else:
+                if(matrixMsg[counter] == "'1'"):
+                    pBoard.setPlayer(row, col, '1')
+                elif(matrixMsg[counter] == "'2'"):
+                    pBoard.setPlayer(row, col, '2')
+                elif(matrixMsg[counter] == "'3'"):
+                    pBoard.setPlayer(row, col, '3')
+            counter += 1
+
+    print("This is after filling: ")
+    print(pBoard)
+    
+
+def handleClient(client, serverChannel, cID, clientele):
+    client.setblocking(1)
+    msg = ""
+    while True:
+        try:
+            msg += client.recv(1024).decode("UTF-8")
+            print("_____start____")
+            print(msg)
+            print("______end_____")
+            command = msg.split("\n")
+            while (len(command) > 1):
+                
+                readyMsg = command[0]
+                msg = "\n".join(command[1:])
+                serverChannel.put(str(cID) + " " + readyMsg)
+                command = msg.split("\n")
+            fillPBoard(msg)
+            msg = "boardFilled " + str(pBoard) + "\n"
+            msg= msg.replace(", ",",")
+ #           msg = "boardFilled Test"
+            print("this is the message to send to client: ",msg)
+            for cID in clientele:
+                print ("this client will get new board: ",repr(cID), repr(playerNum))
+                clientele[cID].send(msg.encode())
+            #client.send(msg.encode())
+        except:
+            # we failed
+            return
+
+def serverThread(clientele, serverChannel):
+    while True:
+        msg = serverChannel.get(True, None)
+        print("msg recv: ", msg)
+        msgList = msg.split(" ")
+        senderID = msgList[0]
+        instruction = msgList[1]
+        details = " ".join(msgList[2:])
+        if (details != ""):
+            for cID in clientele:
+                if cID != senderID:
+                    sendMsg = instruction + " " + senderID + " " + \
+                              details + "\n"
+                    clientele[cID].send(sendMsg.encode())
+                    print("> sent to %s:" % cID, sendMsg[:-1])
+        print()
+        serverChannel.task_done()
+
+clientele = dict()
+playerNum = 0
+
+pBoard = PieceBoard()
+gameOver = False
+
+serverChannel = Queue(100)
+threading.Thread(target = serverThread, args = (clientele, serverChannel)).start()
+
+players = [1, 2, 3]
+
+while True:
+    client, address = server.accept()
+    # myID is the key to the client in the clientele dictionary
+    myID = players[playerNum]
+    print(myID, playerNum)
+    for cID in clientele:
+        print (repr(cID), repr(playerNum))
+        clientele[cID].send(("newPlayer %s\n" % myID).encode())
+        client.send(("newPlayer %s\n" % cID).encode())
+    clientele[myID] = client
+    client.send(("myIDis %s \n" % myID).encode())
+    print("connection recieved from %s" % myID)
+    threading.Thread(target = handleClient, args = 
+                        (client ,serverChannel, myID, clientele)).start()
+    playerNum += 1
